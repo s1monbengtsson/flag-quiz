481c2c3c5ad847b16662e01b5c858d83a04f256e
Simon Bengtsson <simonbengan8@gmail.com>
2022-11-24 16:41:46 +0100

    added students.js and image folder.

---
+++ students.js
@@ -0,0 +1,185 @@
+const students = [
+	{
+		name: "Abdi Ali",
+		image: "assets/images/students/abdi_ali.jpg",
+	},
+	{
+		name: "Albin Lindeborg",
+		image: "assets/images/students/albin_lindeborg.jpg",
+	},
+	{
+		name: "Alexander Olsson",
+		image: "assets/images/students/alexander_olsson.jpg",
+	},
+	{
+		name: "André Lang",
+		image: "assets/images/students/andre_lang.jpg",
+	},
+	{
+		name: "Andreas Gustafsson",
+		image: "assets/images/students/andreas_gustafsson.jpg",
+	},
+	{
+		name: "Andreas Xu",
+		image: "assets/images/students/andreas_xu.jpg",
+	},
+	{
+		name: "Arden Haldorson",
+		image: "assets/images/students/arden_haldorson.jpg",
+	},
+	{
+		name: "Astrid Lindfors",
+		image: "assets/images/students/astrid_lindfors.jpg",
+	},
+	{
+		name: "Bob Oskar Kindgren",
+		image: "assets/images/students/bob_oskar_kindgren.jpg",
+	},
+	{
+		name: "Christine Nilsson",
+		image: "assets/images/students/christine_nilsson.jpg",
+	},
+	{
+		name: "Daniella Dam",
+		image: "assets/images/students/daniella_dam.jpg",
+	},
+	{
+		name: "Eli Ennab",
+		image: "assets/images/students/eli_ennab.jpg",
+	},
+	{
+		name: "Felicia Gartz Levin",
+		image: "assets/images/students/felicia_gartz_levin.jpg",
+	},
+	{
+		name: "Hanna Björling",
+		image: "assets/images/students/hanna_bjorling.jpg",
+	},
+	{
+		name: "Harald Henriksson",
+		image: "assets/images/students/harald_henriksson.jpg",
+	},
+	{
+		name: "Helga Börjesson",
+		image: "assets/images/students/helga_borjesson.jpg",
+	},
+	{
+		name: "Isak Westerberg",
+		image: "assets/images/students/isak_westerberg.jpg",
+	},
+	{
+		name: "Jimmy Nguyen",
+		image: "assets/images/students/jimmy_nguyen.jpg",
+	},
+	{
+		name: "Joakim Ottosson",
+		image: "assets/images/students/joakim_ottosson.jpg",
+	},
+	{
+		name: "Jonas Nilsson",
+		image: "assets/images/students/jonas_nilsson.jpg",
+	},
+	{
+		name: "Kristopher Gray",
+		image: "assets/images/students/kristopher_gray.jpg",
+	},
+	{
+		name: "Linnea Saxvik",
+		image: "assets/images/students/linnea_saxvik.jpg",
+	},
+	{
+		name: "Linus Ohlander",
+		image: "assets/images/students/linus_ohlander.jpg",
+	},
+	{
+		name: "Louise Murray",
+		image: "assets/images/students/louise_murray.jpg",
+	},
+	{
+		name: "Ludvig Lundberg",
+		image: "assets/images/students/ludvig_lundberg.jpg",
+	},
+	{
+		name: "Malin Kyttä",
+		image: "assets/images/students/malin_kytta.jpg",
+	},
+	{
+		name: "Måns Edenfalk",
+		image: "assets/images/students/mans_edenfalk.jpg",
+	},
+	{
+		name: "Marcus Birgersson",
+		image: "assets/images/students/marcus_birgersson.jpg",
+	},
+	{
+		name: "Michelle Klemendz",
+		image: "assets/images/students/michelle_klemendz.jpg",
+	},
+	{
+		name: "Mohammad Mzien",
+		image: "assets/images/students/mohammad_mzien.jpg",
+	},
+	{
+		name: "Nastassia Martin",
+		image: "assets/images/students/nastassia_martin.jpg",
+	},
+	{
+		name: "Patrik He",
+		image: "assets/images/students/patrik_he.jpg",
+	},
+	{
+		name: "Philippe Le Gall",
+		image: "assets/images/students/philippe_legall.jpg",
+	},
+	{
+		name: "Sandra Vidalin",
+		image: "assets/images/students/sandra_vidalin.jpg",
+	},
+	{
+		name: "Simon Bengtsson",
+		image: "assets/images/students/simon_bengtsson.jpg",
+	},
+	{
+		name: "Sofia Travnicek Mattiasson",
+		image: "assets/images/students/sofia_travnicek_mattiasson.jpg",
+	},
+	{
+		name: "Sonja Svidén",
+		image: "assets/images/students/sonja_sviden.jpg",
+	},
+	{
+		name: "Tanja Söderholm",
+		image: "assets/images/students/tanja_soderholm.jpg",
+	},
+	{
+		name: "Viktor Pettersson",
+		image: "assets/images/students/viktor_pettersson.jpg",
+	},
+];
+
+const missing_students = [
+	{
+		name: "Amer El-Jechi",
+		image: null,
+	},
+	{
+		name: "Amin Hassani",
+		image: null,
+	},
+	{
+		name: "Asenakew Zegeye",
+		image: null,
+	},
+	{
+		name: "Gunnar Omander",
+		image: null,
+	},
+	{
+		name: "Louise Keinström",
+		image: null,
+	},
+	{
+		name: "Maryia Tamasheuskaya",
+		image: null,
+	},
+];
