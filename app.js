da323d7d9e4db28aca9ea9acfcd36ca740b3bf10
Simon Bengtsson <simonbengan8@gmail.com>
2022-11-28 18:24:26 +0100

    remade playGame() and added Yates algorithm for randomizing.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // Grabbing elements from DOM
 
-const startGame = document.querySelector('.start-game');
-const toggle = document.querySelector('.game-toggle');
+const startGame = document.querySelector('.game-start');
 const instructions = document.querySelector('.instructions');
 const listEl = document.querySelector('#answer-list');
 const pictureEl = document.querySelector('#image-holder');
 const answersEl = document.querySelector('.answers');
 const questionCounterEl = document.querySelector('#question-counter');
 const roundCounterEl = document.querySelector('#rounds-to-play');
-const tenRounds = document.querySelector('#ten-rounds');
-const twentyRounds = document.querySelector('#twenty-rounds');
-const maxRounds = document.querySelector('#all-rounds');
+const container = document.querySelector('.container');
+const showResults = document.querySelector('#results');
 
-// creating array to store users answers
-const userAnswers = [];
+// Variable that stores the user's answers
+let userAnswers = [];
+
+// Varible that stores the correct answer
+let key = [];
+
+// Array for storing only student names
+let studentNames = [];
 
 // counter for how many rounds has been played
 let roundCounter = 1;
@@ -22,80 +26,118 @@ let roundCounter = 1;
 let roundsToPlay;
 
 
+// Fisher-Yates algorithm for shuffling array
+const shuffleStudents = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    };
+};
+
 // add click events to show number of rounds depending on what user picks
-tenRounds.addEventListener('click', () => {
-    let roundsToPlay = 10;
+startGame.addEventListener('click', (e) => {
+    let roundsToPlay = Number(e.target.innerText);
+    console.log(roundsToPlay);
     playGame();
     questionCounterEl.innerText = `Question: ${roundCounter}/`;
     roundCounterEl.innerText = `${roundsToPlay}`;
 
+    answersEl.addEventListener('click', (e) => {
+        playGame();
+
+        questionCounterEl.innerText = `Question: ${roundCounter + 1} /`;
+        roundCounter++;
+
+        // pushed the guess to an array
+        userAnswers.push(e.target.textContent);
+
+        if (roundCounter > roundsToPlay) {
+            console.log('exiting game');
+            exitGame();
+        }
+    });
 
 });
 
-twentyRounds.addEventListener('click', () => {
-    let roundsToPlay = 20;
-    playGame();
-    questionCounterEl.innerText = `Question: ${roundCounter}/`;
-    roundCounterEl.innerText = `${roundsToPlay}`;
-});
 
-maxRounds.addEventListener('click', () => {
-    let roundsToPlay = 45;
-    playGame();
-    questionCounterEl.innerText = `Question: ${roundCounter}/`;
-    roundCounterEl.innerText = `${roundsToPlay}`;
-});
+
+// twentyRounds.addEventListener('click', () => {
+//     let roundsToPlay = 20;
+//     playGame();
+//     questionCounterEl.innerText = `Question: ${roundCounter}/`;
+//     roundCounterEl.innerText = `${roundsToPlay}`;
+// });
+
+// maxRounds.addEventListener('click', () => {
+//     let roundsToPlay = 41;
+//     playGame();
+//     questionCounterEl.innerText = `Question: ${roundCounter}/`;
+//     roundCounterEl.innerText = `${roundsToPlay}`;
+// });
 
 
 // Function that prints HTML when game is active
-const playGame = (() => {
+const playGame = () => {
     // Empty question and answer before every run
     pictureEl.innerHTML = '';
-    listEl.innerHTML = '';
+    answersEl.innerHTML = '';
 
-    // Shuffling objects in students to make game non-predictable
     const shuffledStudents = students;
 
-    shuffledStudents.sort((a, b) => {
-        return Math.random() - 0.5;
-    });
+    // Shuffling objects in students to make game non-predictable
+    shuffleStudents(shuffledStudents);
 
     // slicing students to pick from a randomized array of 4 objects
     const slicedStudents = shuffledStudents.slice(0, 4);
-    console.log(slicedStudents);
+    // console.log('sliced students, before shuffling', slicedStudents);
 
-    // Map pictured from sliced array
-    const getPictures = slicedStudents.map(student => student.image);
-    console.log(slicedStudents[0]);
+    // variable for the right answer, and sets the image on display
+    let correctStudent = shuffledStudents[0];
 
-    // Shuffle ONLY pictures so picture and correct answer is not the same index
-    const randomPicture = getPictures.sort((a, b) => {
-        return Math.random() - 0.5;
+    key.push(correctStudent);
+
+    pictureEl.innerHTML = `<img src=${correctStudent.image}>`
+    shuffledStudents.shift();
+    console.log('removed:', shuffledStudents.shift());
+
+    // console.log('correct student:', correctStudent);
+
+    // Shuffle the sliced array once again to make the game more randomized
+    shuffleStudents(slicedStudents);
+    // console.log('sliced students, but after shuffle', slicedStudents);
+
+    // Creating new array that only contains students name
+    studentNames = slicedStudents.map(student => student.name);
+    console.log('studentNames:', studentNames);
+
+    // Print options to DOM
+    studentNames.forEach(student => {
+        answersEl.innerHTML += `<button class="btn btn-warning m-2 p-3 col-5">${student}</button>`
     });
 
-
+    // Hide instructions while game is active
     instructions.style.display = "none";
-    pictureEl.innerHTML = `<img src=${randomPicture[0]}>`
-    listEl.innerHTML += `<button class="answer-item row col-5 m-2 px-5 d-flex justify-content-center align-items-center btn btn-primary">${slicedStudents[0].name} </button>`
-    listEl.innerHTML += `<button class="answer-item row col-5 m-2 px-5 d-flex justify-content-center align-items-center btn btn-primary">${slicedStudents[1].name}</button>`
-    listEl.innerHTML += `<button class="answer-item row col-5 m-2 px-5 d-flex justify-content-center align-items-center btn btn-primary">${slicedStudents[2].name}</button>`
-    listEl.innerHTML += `<button class="answer-item row col-5 m-2 px-5 d-flex justify-content-center align-items-center btn btn-primary">${slicedStudents[3].name}</button>`
-});
+
+    // Once students X has been displayed on picture, remove student X from array
+
+
+};
 
 // Click event for starting the game
-startGame.addEventListener('click', () => {
-    playGame();
-});
 
-answersEl.addEventListener('click', (e) => {
-    playGame();
 
-    questionCounterEl.innerText = `Question: ${roundCounter + 1}/`;
-    roundCounter++;
 
-    // pushed the guess to an array
-    userAnswers.push(e.target.textContent);
-});
+
+
+// Exit game
+
+const exitGame = () => {
+    container.style.display = "none";
+    showResults.innerHTML += `<p>${userAnswers}</p>`;
+    showResults.innerHTML += `<p>Correct answers will go here:</p>`;
+};
 
 
 
