560f96d1a446ce623ae38556af7730ff576f4b2d
Simon Bengtsson <dev.simon.bengtsson@gmail.com>
2023-01-17 18:33:06 +0100

    corrected misspelling

--- flags.js
+++ flags.js
@@ -2,727 +2,582 @@ const flags = [
     {
         country: "Argentina",
         image: "assets/argentina.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Albania",
         image: "assets/albania.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Angola",
         image: "assets/angola.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Australia",
         image: "assets/australia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Austria",
         image: "assets/austria.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Azerbaijan",
         image: "assets/azerbaijan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Bahrain",
         image: "assets/bahrain.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Belgium",
         image: "assets/belgium.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Bolivia",
         image: "assets/bolivia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Botswana",
         image: "assets/botswana.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Brazil",
         image: "assets/brazil.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Bulgaria",
         image: "assets/bulgaria.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Cambodia",
         image: "assets/cambodia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Cameroon",
         image: "assets/cameroon.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Canada",
         image: "assets/canada.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Cape Verde",
         image: "assets/cape_verde.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Chad",
         image: "assets/chad.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Chile",
         image: "assets/chile.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "China",
         image: "assets/china.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Colombia",
         image: "assets/colombia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Costa Rica",
         image: "assets/costa_rica.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Croatia",
         image: "assets/croatia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Cuba",
         image: "assets/cuba.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Cyprus",
         image: "assets/cyprus.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Czech Republic",
         image: "assets/czech_republic.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Denmark",
         image: "assets/denmark.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Dominican Republic",
         image: "assets/dominican_republic.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Democratic Republic Congo",
         image: "assets/dr_congo.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Ecuador",
         image: "assets/ecuador.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Egypt",
         image: "assets/egypt.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "El Salvador",
         image: "assets/el_salvador.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "England",
         image: "assets/england.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Estonia",
         image: "assets/estonia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Finland",
         image: "assets/finland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "France",
         image: "assets/france.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Gabon",
         image: "assets/gabon.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Gambia",
         image: "assets/gambia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Georgia",
         image: "assets/georgia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Germany",
         image: "assets/germany.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Ghana",
         image: "assets/ghana.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Gibraltar",
         image: "assets/gibraltar.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Greece",
         image: "assets/greece.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Guatemala",
         image: "assets/guatemala.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Guinea",
         image: "assets/guinea.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Haiti",
         image: "assets/haiti.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Honduras",
         image: "assets/honduras.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Hungary",
         image: "assets/hungary.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Iceland",
         image: "assets/iceland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "India",
         image: "assets/india.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Indonesia",
         image: "assets/indonesia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Iran",
         image: "assets/iran.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Iraq",
         image: "assets/iraq.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Ireland",
         image: "assets/ireland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Italy",
         image: "assets/italy.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Ivory Coast",
         image: "assets/ivory_coast.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Jamaica",
         image: "assets/jamaica.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Japan",
         image: "assets/japan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Kazakhstan",
         image: "assets/kazakhstan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Kenya",
         image: "assets/kenya.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Laos",
         image: "assets/laos.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Latvia",
         image: "assets/latvia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Lebanon",
         image: "assets/lebanon.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Lesotho",
         image: "assets/lesotho.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Liechtenstein",
         image: "assets/liechtenstein.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Lithuania",
         image: "assets/lithuania.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Luxembourg",
-        image: "assets/luxembourgh.png",
-        hint: "Won the FIFA World Cup 2022"
+        image: "assets/luxembourg.png",
     },
     {
         country: "Madagascar",
         image: "assets/madagascar.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Malawi",
         image: "assets/malawi.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Malaysia",
         image: "assets/malaysia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Maldives",
         image: "assets/maldives.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Malta",
         image: "assets/malta.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Mauritius",
         image: "assets/mauritius.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Mexico",
         image: "assets/mexico.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Micronesia",
         image: "assets/micronesia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Moldova",
         image: "assets/moldova.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Mongolia",
         image: "assets/mongolia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Montenegro",
         image: "assets/montenegro.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Morocco",
         image: "assets/morocco.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Myanmar",
         image: "assets/myanmar.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Nepal",
         image: "assets/nepal.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Netherlands",
         image: "assets/netherlands.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "New Zealand",
         image: "assets/new_zealand.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Nicaragua",
         image: "assets/nicaragua.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Nigeria",
         image: "assets/nigeria.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Norway",
         image: "assets/norway.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Oman",
         image: "assets/oman.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
-        country: "Pakiastan",
+        country: "Pakistan",
         image: "assets/pakistan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Palau",
         image: "assets/palau.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Panama",
         image: "assets/panama.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Papua New Guinea",
         image: "assets/papua_new_guinea.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Paraguay",
         image: "assets/paraguay.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Peru",
         image: "assets/peru.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Philippines",
         image: "assets/philippines.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Poland",
         image: "assets/poland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Portugal",
         image: "assets/portugal.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Puerto Rico",
         image: "assets/puerto_rico.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Qatar",
         image: "assets/qatar.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Romania",
         image: "assets/romania.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Rwanda",
         image: "assets/rwanda.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Samoa",
         image: "assets/samoa.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "San Marino",
         image: "assets/san_marino.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Saudi Arabia",
         image: "assets/saudi_arabia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Scotland",
         image: "assets/scotland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Senegal",
         image: "assets/senegal.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Serbia",
         image: "assets/serbia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Seychelles",
         image: "assets/seychelles.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Sierra Leone",
         image: "assets/sierra_leone.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Singapore",
         image: "assets/singapore.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Slovakia",
         image: "assets/slovakia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Slovenia",
         image: "assets/slovenia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Somalia",
         image: "assets/somalia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "South Africa",
         image: "assets/south_africa.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "South Korea",
         image: "assets/south_korea.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "South Sudan",
         image: "assets/south_sudan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Spain",
         image: "assets/spain.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Sri Lanka",
         image: "assets/sri_lanka.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Sudan",
         image: "assets/sudan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Swaziland",
         image: "assets/swaziland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Sweden",
         image: "assets/sweden.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Switzerland",
         image: "assets/switzerland.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Syria",
         image: "assets/syria.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Taiwan",
         image: "assets/taiwan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Tanzania",
         image: "assets/tanzania.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Thailand",
         image: "assets/thailand.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Togo",
         image: "assets/togo.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Trinidad and Tobago",
         image: "assets/trinidad_and_tobago.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Tunisia",
         image: "assets/tunisia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Turkey",
         image: "assets/turkey.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Turkmenistan",
         image: "assets/turkmenistan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Uganda",
         image: "assets/uganda.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Ukraine",
         image: "assets/ukraine.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "United Arab Emirates",
         image: "assets/united_arab_emirates.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "United Kingdom",
         image: "assets/united_kingdom.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "United States of America",
         image: "assets/united_states_of_america.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Uruguay",
         image: "assets/uruguay.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Uzbekistan",
         image: "assets/uzbekistan.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Vatican City",
         image: "assets/vatican_city.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Venezuela",
         image: "assets/venezuela.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Vietnam",
         image: "assets/vietnam.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Virgin Islands",
         image: "assets/virgin_islands.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Wales",
         image: "assets/wales.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Western Sahara",
         image: "assets/western_sahara.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Yemen",
         image: "assets/yemen.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Zambia",
         image: "assets/zambia.png",
-        hint: "Won the FIFA World Cup 2022"
     },
     {
         country: "Zimbabwe",
         image: "assets/zimbabwe.png",
-        hint: "Won the FIFA World Cup 2022"
     },
 
 
